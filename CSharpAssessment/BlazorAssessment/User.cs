6891993d6127f5189d3a62931e949de5fd0ede8e
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2024-08-06 00:11:35 +0200

    :tada: Add: BlazorAssessment
    Add: BlazorAssessment

--- CSharpAssessment/BlazorAssessment/User.cs
+++ CSharpAssessment/BlazorAssessment/User.cs
@@ -2,5 +2,22 @@ namespace BlazorAssessment;
 
 public class User
 {
-    
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateTime LastLoginDate { get; set; }
+
+    public User(string userName, string email, DateTime lastLoginDateTime)
+    {
+        Id = Guid.NewGuid();
+        Name = userName;
+        Email = email;
+        LastLoginDate = lastLoginDateTime;
+    }
+
+    public User()
+    {
+        
+        
+    }
 }
