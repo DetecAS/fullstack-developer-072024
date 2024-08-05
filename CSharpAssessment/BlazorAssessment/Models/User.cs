42791ba937fb1d9da103a3e5dc84c221010e684c
Zekaria Abdirahman Mohamed <zekariaam@student.uia.no>
2024-08-06 00:20:42 +0200

    :lipstick: Add: Models Folder
    Add: Models Folder

--- CSharpAssessment/BlazorAssessment/Models/User.cs
+++ CSharpAssessment/BlazorAssessment/Models/User.cs
@@ -1,7 +1,9 @@
-namespace BlazorAssessment;
+namespace BlazorAssessment.Models;
 
 public class User
 {
+
+
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
